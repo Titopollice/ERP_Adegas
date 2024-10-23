966aea8d5e234804a3a12e100d3801622bbea1da
Tiago Oliveira <92068949+Titopollice@users.noreply.github.com>
2024-10-22 22:34:46 -0300

    Ajuste Idenftificação da Tela

--- src/pages/Usuario.jsx
+++ src/pages/Usuario.jsx
@@ -206,7 +206,7 @@ const Usuario = () => {
     <div className="users-container">
       <ToastContainer />
       <header className="products-header flex justify-between items-center p-6">
-        <h1 className="text-xl font-bold">Estoque</h1>
+        <h1 className="text-xl font-bold">Usuario</h1>
         <div className="flex items-center space-x-4">
           <span className="text-whrite-800">{userName}</span>
           <div className="flex items-center space-x-2">

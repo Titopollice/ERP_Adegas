87af48ee1e3cb3c1f83ce492d9e14a45a036c917
Tiago Oliveira <92068949+Titopollice@users.noreply.github.com>
2024-10-30 20:36:49 -0300

    Tentativa de deploy vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
   },
-  define:{'import.meta.env.VITE_APP_URL_BACKEND': JSON.stringify(process.env.VITE_APP_URL_BACKEND)}
+  define: {
+    "import.meta.env.VITE_APP_URL_BACKEND": JSON.stringify(
+      process.env.VITE_APP_URL_BACKEND
+    ),
+  },
+  base: "/",
 });

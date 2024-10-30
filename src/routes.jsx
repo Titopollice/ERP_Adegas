b1d34bff6ce54f1b16ad4f8371f8d7e214a44982
Tiago Oliveira <92068949+Titopollice@users.noreply.github.com>
2024-10-30 20:28:27 -0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -16,108 +16,110 @@ import RelatorioEstoque from "./pages/RelatorioEstoque";
 import RelatorioReceber from "./pages/RelatorioReceber";
 import Cliente from "./pages/Cliente";
 import ResetPassword from "./pages/ResetPassword";
-import ProtectedRoute from './pages/ProtectedRoute'; // Importe o componente de rota protegida
+import ProtectedRoute from "./pages/ProtectedRoute"; // Importe o componente de rota protegida
 
 const AppRoutes = () => (
   <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/Home" element={<Home />} />
-    
-    {/* Rota protegida para Vendas */}
-    <Route
-      path="/vendas"
-      element={
-        <ProtectedRoute>
-          <Vendas />
-        </ProtectedRoute>
-      }
-    />
-    
-    {/* Outras rotas que precisam ser protegidas */}
-    <Route
-      path="/produtos"
-      element={
-        <ProtectedRoute>
-          <Produtos />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/usuario"
-      element={
-        <ProtectedRoute>
-          <Usuario />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/fornecedor"
-      element={
-        <ProtectedRoute>
-          <Fornecedor />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/contasReceber"
-      element={
-        <ProtectedRoute>
-          <ContasReceber />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/ContasPagar"
-      element={
-        <ProtectedRoute>
-          <ContasPagar />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/estoque"
-      element={
-        <ProtectedRoute>
-          <Estoque />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/relatorioVendas"
-      element={
-        <ProtectedRoute>
-          <RelatorioVendas />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/relatorioPagar"
-      element={
-        <ProtectedRoute>
-          <RelatorioPagar />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/relatorioEstoque"
-      element={
-        <ProtectedRoute>
-          <RelatorioEstoque />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path="/relatorioReceber"
-      element={
-        <ProtectedRoute>
-          <RelatorioReceber />
-        </ProtectedRoute>
-      }
-    />
-    
-    {/* Rotas não protegidas */}
-    <Route path="/cliente" element={<Cliente />} />
-    <Route path="/reset-password/:token" element={<ResetPassword />} />
+    <Route path="/">
+      <Route index element={<Login />} />
+      <Route path="/Home" element={<Home />} />
+
+      {/* Rota protegida para Vendas */}
+      <Route
+        path="/vendas"
+        element={
+          <ProtectedRoute>
+            <Vendas />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Outras rotas que precisam ser protegidas */}
+      <Route
+        path="/produtos"
+        element={
+          <ProtectedRoute>
+            <Produtos />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/usuario"
+        element={
+          <ProtectedRoute>
+            <Usuario />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/fornecedor"
+        element={
+          <ProtectedRoute>
+            <Fornecedor />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/contasReceber"
+        element={
+          <ProtectedRoute>
+            <ContasReceber />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/ContasPagar"
+        element={
+          <ProtectedRoute>
+            <ContasPagar />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/estoque"
+        element={
+          <ProtectedRoute>
+            <Estoque />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/relatorioVendas"
+        element={
+          <ProtectedRoute>
+            <RelatorioVendas />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/relatorioPagar"
+        element={
+          <ProtectedRoute>
+            <RelatorioPagar />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/relatorioEstoque"
+        element={
+          <ProtectedRoute>
+            <RelatorioEstoque />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/relatorioReceber"
+        element={
+          <ProtectedRoute>
+            <RelatorioReceber />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Rotas não protegidas */}
+      <Route path="/cliente" element={<Cliente />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+    </Route>
   </Routes>
 );
 
